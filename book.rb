d64458c8221cc5e00e05f78a886aead3a9447943
Anas Abubakar <anasabubakar2015@gmail.com>
2022-09-29 17:54:33 +0100

    Create rental class with associations

---
+++ book.rb
@@ -0,0 +1,15 @@
+class Book
+  attr_reader :rental
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rental = []
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.book = self
+  end
+end
